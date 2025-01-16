b257291a9a0a881308e3de276aadd82adb9f58ba
Austin Erhart <austinerhartm@gmail.com>
2025-01-15 21:36:03 -0600

    Removed unnecessary files, added .gitignore, and created a homepage.js and .css for styling
    Created a baseplate homepage for creating and added routing to the app.js in order to switch between webpages. Added a .gitignore to debloat the pushes.

---
+++ bravechild/src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+
+    return (
+        <div className="page">
+            <div className="overlay">
+                <h1>BraveChild</h1>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
